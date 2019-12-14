9cbc8cce15915452819bb082b0ab226c9ad2b6a3
Kanchan Tyagi <kanchan.tyagi8@gmail.com>
2019-12-14 16:16:03 +0530

    Updates

---
+++ precache-manifest.2e3b329985d6d60ec33cbeafaad9c493.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b15bc47948289f66fda4720ae18b39",
+    "url": "/react-deployment/index.html"
+  },
+  {
+    "revision": "26ebbe25cfe0a1d1471e",
+    "url": "/react-deployment/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e200f040fb5a7c806dc5",
+    "url": "/react-deployment/static/js/2.e95ce62f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-deployment/static/js/2.e95ce62f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26ebbe25cfe0a1d1471e",
+    "url": "/react-deployment/static/js/main.1f4cf987.chunk.js"
+  },
+  {
+    "revision": "3bc8747b46974ff79faa",
+    "url": "/react-deployment/static/js/runtime-main.d89d9a41.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deployment/static/media/logo.5d5d9eef.svg"
+  }
+]);
